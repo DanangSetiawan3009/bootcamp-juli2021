8b18e294d83f641e2d0720a624c3249e8fc41902
Wawan S <wawan@g2academy.co>
2021-10-02 12:58:34 +0700

    login example'

---
+++ src/components/rowInput/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+
+
+class RowInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        // const a = {
+        //     name: "Admin",
+        //     age: 21,
+        //     address: "Jakarta"
+        // }
+        // const { name, age, address } = a
+        // console.log(address);
+        // const label = this.props.label
+        // const type = this.props.type
+        const { label, type, name, change } = this.props
+        return (
+            <div className="row-input">
+                <span>{label}</span>
+                <input
+                    type={type}
+                    name={name}
+                    onChange={change}
+                />
+            </div>
+        );
+    }
+}
+
+export default RowInput;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { RowInput } from "../../components"
 import "./login.css"
 
 
@@ -6,27 +7,50 @@ import "./login.css"
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            username: "",
+            password: "",
+            isLogin: false
+        }
     }
+
+    setValue = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    loginButton = () => {
+        const { username, password } = this.state
+        if (username === "admin" && password === "1234")
+            this.setState({ isLogin: true })
+        else alert("Invalid username or password!!")
+    }
+
     render() {
         return (
             <div className="login-containter">
                 <fieldset>
                     <legend>Value</legend>
-                    <h1>Username: </h1>
-                    <h1>Password: </h1>
+                    <h1>Username: {this.state.username}</h1>
+                    <h1>Password: {this.state.password}</h1>
+                    <h1>Status Login: {this.state.isLogin.toString()}</h1>
                 </fieldset>
                 <div className="login-containter">
-                    <div className="row-input">
-                        <span>Username</span>
-                        <input type="text" />
-                    </div>
-                    <div className="row-input">
-                        <span>Password</span>
-                        <input type="password" />
-                    </div>
+                    <RowInput
+                        label="Username"
+                        type="text"
+                        name="username"
+                        change={this.setValue}
+                    />
+                    <RowInput
+                        label="Password"
+                        type="password"
+                        name="password"
+                        change={this.setValue}
+                    />
                     <div className="row-button">
-                        <button>Log In</button>
+                        <button onClick={this.loginButton}>Log In</button>
                     </div>
                 </div>
             </div>
